e41d9d44b0277aa18beae248d6f442dddb07fea6
asawaswapnil <asawaswapnil@gmail.com>
2019-05-11 11:10:46 -0400

    first copy

---
+++ ConceptExtractor/README.md
@@ -0,0 +1,32 @@
+python 3
+
+## This project extracts concepts with different mechanisms
+
+TFIDF <br />
+TFIDFNP<br />
+LDA<br />
+wikipedia titles as concepts<br />
+wikipedia sections as concepts<br />
+ACM publication Keywords as concepts<br />
+Global Ngrams<br />
+<br />
+
+## Also it has possiblity to learn representation for these concepts using
+
+Concept2Vec<br />
+Doc2Vec<br />
+Word2Vec<br />
+
+---
+
+
+## For using concept_extractor<br />
+-  You have to have a text file  with text in : IR_CORPUS = 'add the path to your file here'<br />
+- And uncomment the concept method you want to run<br />
+- The concepts will be generated in folder keyphrase_output
+
+
+## Failure of Marisa Trie ... point explicity to C path
+
+export MACOSX_DEPLOYMENT_TARGET=10.9
+export CPATH=/Library/Developer/CommandLineTools/usr/include/c++/v1
---
+++ ConceptExtractor/entity/concept_extractor.py
@@ -0,0 +1,139 @@
+# encoding: utf-8
+from __future__ import generators
+import os
+from entity.util.config import config
+from entity.util import document as dc
+from entity.util import common as cm
+from entity.models import tfidf,keyword
+from entity.util.extractors import extract_top_kcs_sm,extract_top_kcs
+
+
+if __name__=='__main__':
+
+
+    BOOK_CORPUS = 'data/sampleSyllabus.csv'
+    listbooks = []
+
+    concept = config.LIST_FILTER
+    keyword_list_path = config.irbook_glossary_list
+
+
+    bookdocs_16 = dc.load_document(BOOK_CORPUS,
+                                          booknames=[],
+                                          textfield=['text'],
+                                          idfield="docid",
+                                          otherfields=['bookname'],
+                                          booknamefield='bookname',
+                                   )
+
+    train_docs = bookdocs_16
+    extract_docs = bookdocs_16
+    model_dir = "model/"
+    concept_dir = "concepts/"
+    Define_kc = False
+    no_of_topics = 10
+
+
+
+    if concept == config.TFIDFNP:
+
+        ptfidf =  model_dir +"m_"+ concept +".pickle"
+        outdir =concept_dir + config.dir_sep + concept
+
+        if no_of_topics == -1:
+            outdir += "all" + config.file_ext
+        else:
+            outdir += "top" + str(no_of_topics) + config.file_ext
+
+        model = None
+
+        if not os.path.exists(ptfidf) or config.Remove_Prev_models:
+            model = tfidf.TFIDFExtractor(train_docs,ngram=(1,5),mindf=1)
+            model.train()
+            model.save_model(ptfidf)
+        else:
+            print(" Model Exists : "+ptfidf)
+
+        model = cm.load_model(ptfidf)
+
+        doc2concepts = model.get_Topics_npFilter(extract_docs)
+
+        df = extract_top_kcs(doc2concepts=doc2concepts,output_dir=outdir,define_kc=False,topk=no_of_topics)
+
+    if concept == config.TFIDF:
+
+        ptfidf =  model_dir +"m_"+ concept +".pickle"
+        outdir =concept_dir + config.dir_sep + concept
+
+        if no_of_topics == -1:
+            outdir += "all" + config.file_ext
+        else:
+            outdir += "top" + str(no_of_topics) + config.file_ext
+
+        model = None
+
+        if not os.path.exists(ptfidf)  or config.Remove_Prev_models:
+            model = tfidf.TFIDFExtractor(train_docs,ngram=(1,5),mindf=1)
+            model.train()
+            model.save_model(ptfidf)
+        else:
+            print(" Model Exists : "+ptfidf)
+
+        model = cm.load_model(ptfidf)
+
+        doc2concepts = model.get_Topics(extract_docs)
+
+        df = extract_top_kcs(doc2concepts=doc2concepts,output_dir=outdir,define_kc=False,topk=no_of_topics)
+
+    if concept == config.NGRAMS:
+
+        ptfidf =  model_dir +"m_"+ concept +".pickle"
+        outdir =concept_dir + config.dir_sep + concept
+
+        if no_of_topics == -1:
+            outdir += "all" + config.file_ext
+        else:
+            outdir += "top" + str(no_of_topics) + config.file_ext
+
+        model = None
+
+        if not os.path.exists(ptfidf)  or config.Remove_Prev_models:
+            model = tfidf.TFIDFExtractor(train_docs,ngram=(1,5),mindf=1)
+            model.train()
+            model.save_model(ptfidf)
+        else:
+            print(" Model Exists : "+ptfidf)
+
+        model = cm.load_model(ptfidf)
+        doc2concepts = model.get_Topics(extract_docs)
+        df = extract_top_kcs(doc2concepts=doc2concepts,output_dir=outdir,define_kc=False,topk=-1)
+
+    if concept == config.LIST_FILTER:
+        ingram = (1,5)
+        name = "irbook_tfidf_keyword"
+        ptfidf =  model_dir +"m_"+ concept+"ngram"+str(ingram[0])+"_"+str(ingram[1])+".pickle"
+        outdir =concept_dir + config.dir_sep + concept+"_"+name
+
+        if no_of_topics == -1:
+            outdir += "all"
+        else:
+            outdir += "top" + str(no_of_topics)
+
+        model = None
+
+        if not os.path.exists(ptfidf)  or config.Remove_Prev_models:
+            model = tfidf.TFIDFExtractor(train_docs,ngram=ingram)
+            model.train()
+            model.save_model(ptfidf)
+        else:
+            print(" Model Exists : "+ptfidf)
+
+        model = cm.load_model(ptfidf)
+        keyword_list = keyword.KeywordList(name,keyword_list_path)
+        doc2concepts_ngram = model.get_Topics(extract_docs)
+        doc2concepts = keyword_list.get_Topics_secondFilter(doc2concepts_ngram)
+
+
+        df = extract_top_kcs(doc2concepts=doc2concepts,output_dir=outdir,define_kc=False,topk=no_of_topics)
+        # df = extract_top_kcs_sm(doc2concepts=doc2concepts,define_kc=False,topk=no_of_topics)
+        # df.to_csv(outdir,index=None)
